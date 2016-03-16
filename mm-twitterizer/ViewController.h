ea095c705e2b609033dbde152ac5c87e7bb768da
Chris Serra <csserra@gmail.com@gmail.com>
2016-03-16 10:30:45 -0700

    Initial Commit

---
+++ mm-twitterizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mm-twitterizer
+//
+//  Created by Christopher Serra on 3/16/16.
+//  Copyright © 2016 plugh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mm-twitterizer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mm-twitterizer
+//
+//  Created by Christopher Serra on 3/16/16.
+//  Copyright © 2016 plugh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
